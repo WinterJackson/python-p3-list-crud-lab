baa563c02d87e3b3a0ef874a8b331c119d37d859
WinterJackson <winterjacksonwj@gmail.com>
2023-08-23 08:43:00 +0300

    Completed Assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,43 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    new_list = [1, 'apple', True, 3.14]
+    return new_list
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    return my_list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(my_list, element):
+    my_list.insert(0, element)
+    return my_list
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(my_list):
+    if my_list:
+        my_list.pop()
+    return my_list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(my_list):
+    if my_list:
+        del my_list[0]
+    return my_list
+    
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(lst):
+    if lst:
+        return lst[0]
+    else:
+        return None 
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(lst, index):
+    if 0 <= index < len(lst):
+        return lst[index]
+    else:
+        return None
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(lst):
+    if lst:
+        return lst[-1]
+    else:
+        return None
